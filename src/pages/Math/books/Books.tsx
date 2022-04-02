0bced8c9fbd321859c97a819a96019e66bf35094
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-04-03 01:51:38 +0300

    deleted some code

--- src/pages/Math/books/Books.tsx
+++ src/pages/Math/books/Books.tsx
@@ -1,17 +1,6 @@
 import React from 'react'
-// import styles from './books.module.scss'
 
 import Grid from '@mui/material/Grid'
-import Accordion from '@mui/material/Accordion'
-import AccordionSummary from '@mui/material/AccordionSummary'
-import AccordionDetails from '@mui/material/AccordionDetails'
-import Typography from '@mui/material/Typography'
-import Avatar from '@mui/material/Avatar'
-import Stack from '@mui/material/Stack'
-
-import ArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
-
-import math2Form from './img/math-2.jpg'
 import SchoolCard from 'components/schoolCard/SchoolCard'
 import { schoolBooks2Form, schoolBooks4Form } from '../../Schoolbooks/books'
 
